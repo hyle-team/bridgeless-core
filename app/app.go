a1d1bfe9ce144df7b64f6b3fbb6d8f2266b13fd9
Mark Cherepovskyi <mark.cherepovskyi@gmail.com>
2024-10-08 15:41:32 +0300

    12.1.12-rc1

--- app/app.go
+++ app/app.go
@@ -890,6 +890,13 @@ func NewBridge(
 		},
 	)
 
+	app.UpgradeKeeper.SetUpgradeHandler(
+		"v12.1.12-rc1",
+		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		},
+	)
+
 	if loadLatest {
 		if err := app.LoadLatestVersion(); err != nil {
 			tmos.Exit(err.Error())
