a6db489446d3e434331f20fe69df842f3f89006a
DrLivsey00 <emihrin50@gmail.com>
2024-11-18 13:43:38 +0200

    CLI Voting tx`s implemented

--- x/multisig/client/cli/tx.go
+++ x/multisig/client/cli/tx.go
@@ -33,6 +33,7 @@ func GetTxCmd() *cobra.Command {
 	cmd.AddCommand(
 		TxGroupsCmd(),
 		TxProposalsCmd(),
+		TxVoteCmd(),
 	)
 	// this line is used by starport scaffolding # 1
 	return cmd
---
+++ x/multisig/client/cli/tx_votes.go
@@ -0,0 +1,69 @@
+package cli
+
+import (
+	"fmt"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/hyle-team/bridgeless-core/v12/x/multisig/types"
+	"github.com/spf13/cobra"
+	"strconv"
+)
+
+func TxVoteCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        "vote",
+		Short:                      "proposals subcommands",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+	cmd.AddCommand(
+		CmdNewVote())
+	return cmd
+}
+
+func CmdNewVote() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "vote [creator_address] [proposal_id] [option]",
+		Short: "Create a new vote",
+		Args:  cobra.ExactArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.Flags().Set(flags.FlagFrom, args[0])
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			proposalId, err := strconv.ParseUint(args[1], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			vote_option, err := strconv.ParseUint(args[2], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			if vote_option != 0 && vote_option != 1 {
+				return fmt.Errorf("vote option must be 0 or 1")
+			}
+
+			opt := types.VoteOption_NO
+			if vote_option == 0 {
+				opt = types.VoteOption_YES
+			}
+
+			msg := types.NewMsgVote(clientCtx.GetFromAddress().String(), proposalId, opt)
+
+			if err = msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
