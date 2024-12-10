2ee534cc439493b7095e7ba7b31a9438510f8d00
DrLivsey00 <emihrin50@gmail.com>
2024-12-10 16:56:07 +0200

    Genesis update

--- x/tracking/genesis.go
+++ x/tracking/genesis.go
@@ -2,6 +2,8 @@ package tracking
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/hyle-team/bridgeless-core/v12/x/tracking/keeper"
 	"github.com/hyle-team/bridgeless-core/v12/x/tracking/types"
 )
@@ -10,12 +12,25 @@ import (
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
 	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
+
+	// import positions
+	for _, position := range genState.Positions {
+		k.SetPosition(ctx, position.Address, position)
+	}
+
 }
 
 // ExportGenesis returns the module's exported genesis
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
 	genesis.Params = k.GetParams(ctx)
+
+	// export positions
+	positions, _, err := k.GetPositionsWithPagination(ctx, &query.PageRequest{Limit: query.MaxLimit})
+	if err != nil {
+		panic(sdkerrors.Wrap(err, "failed to export positions"))
+	}
+	genesis.Positions = positions
 
 	// this line is used by starport scaffolding # genesis/module/export
 
--- x/tracking/types/params.go
+++ x/tracking/types/params.go
@@ -1,6 +1,9 @@
 package types
 
 import (
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 )
 
@@ -25,6 +28,11 @@ func DefaultParams() Params {
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
 	return paramtypes.ParamSetPairs{
 		paramtypes.NewParamSetPair([]byte(BorrowedEventNameKey), &p.BorrowEventName, validateBorrowedEventName),
+		paramtypes.NewParamSetPair([]byte(ContractAddress), &p.ContractAddress, validateContractAddress),
+		paramtypes.NewParamSetPair([]byte(LiquidatorAddress), &p.LiquidatorAddress, validateLiquidatorAddress),
+		paramtypes.NewParamSetPair([]byte(SenderAddress), &p.SenderAddress, validateSenderAddress),
+		paramtypes.NewParamSetPair([]byte(LiquidationEventName), &p.LiquidationEventName, validateLiquidationEventName),
+		paramtypes.NewParamSetPair([]byte(OracleAddress), &p.OracleAddress, validateOracleAddress),
 	}
 }
 
@@ -34,5 +42,74 @@ func (p Params) Validate() error {
 }
 
 func validateBorrowedEventName(i interface{}) error {
+	name, ok := i.(string)
+	if !ok {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "invalid parameter type: %T", i)
+	}
+
+	if name == "" {
+		return sdkerrors.Wrap(fmt.Errorf("empty string"), "borrow event name cannot be an empty string")
+	}
+	return nil
+}
+
+func validateContractAddress(i interface{}) error {
+	adr, ok := i.(string)
+	if !ok {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "invalid type: %T", i)
+	}
+
+	if _, err := sdk.AccAddressFromHexUnsafe(adr); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid contract address: %s", err.Error())
+	}
+	return nil
+}
+
+func validateLiquidatorAddress(i interface{}) error {
+	liq, ok := i.(string)
+	if !ok {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "invalid parameter type: %T", i)
+	}
+
+	if _, err := sdk.AccAddressFromBech32(liq); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid liquidator address: %s", err.Error())
+	}
+
+	return nil
+}
+
+func validateSenderAddress(i interface{}) error {
+	sender, ok := i.(string)
+	if !ok {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "invalid parameter type: %T", i)
+	}
+
+	if _, err := sdk.AccAddressFromHexUnsafe(sender); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid sender address: %s", err.Error())
+	}
+	return nil
+}
+
+func validateLiquidationEventName(i interface{}) error {
+	name, ok := i.(string)
+	if !ok {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "invalid parameter type: %T", i)
+	}
+
+	if name == "" {
+		return sdkerrors.Wrap(fmt.Errorf("empty string"), "liquidation event name cannot be an empty string")
+	}
+	return nil
+}
+
+func validateOracleAddress(i interface{}) error {
+	oracle, ok := i.(string)
+	if !ok {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "invalid parameter type: %T", i)
+	}
+
+	if _, err := sdk.AccAddressFromHexUnsafe(oracle); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid oracle address: %s", err.Error())
+	}
 	return nil
 }
