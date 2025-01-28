1943d84af6862ccef27c74196cace5bfadf8bb24
EduardMikhrin <emihrin50@gmail.com>
2025-01-28 14:08:07 -0800

    added deposit submitted event

--- x/bridge/keeper/msg_transactions.go
+++ x/bridge/keeper/msg_transactions.go
@@ -5,6 +5,8 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/hyle-team/bridgeless-core/v12/x/bridge/types"
+	"math/big"
+	"strconv"
 )
 
 func (m msgServer) SubmitTransactions(goCtx context.Context, msg *types.MsgSubmitTransactions) (*types.MsgSubmitTransactionsResponse, error) {
@@ -34,7 +36,28 @@ func (m msgServer) SubmitTransactions(goCtx context.Context, msg *types.MsgSubmi
 
 		m.SetTransaction(ctx, tx)
 
+		// emit submit deposit event to notify users about new submitted deposit
+		emitSubmitEvent(ctx, tx)
+
 	}
 
 	return &types.MsgSubmitTransactionsResponse{}, nil
+}
+
+func emitSubmitEvent(sdkCtx sdk.Context, transaction types.Transaction) {
+	sdkCtx.EventManager().EmitEvent(sdk.NewEvent(types.EventType_DEPOSIT_SUBMITTED.String(),
+		sdk.NewAttribute(types.AttributeKeyDepositTxHash, transaction.DepositTxHash),
+		sdk.NewAttribute(types.AttributeKeyDepositNonce, big.NewInt(int64(transaction.DepositTxIndex)).String()),
+		sdk.NewAttribute(types.AttributeKeyDepositChainId, transaction.DepositChainId),
+		sdk.NewAttribute(types.AttributeKeyDepositAmount, transaction.DepositAmount),
+		sdk.NewAttribute(types.AttributeKeyDepositBlock, big.NewInt(int64(transaction.DepositBlock)).String()),
+		sdk.NewAttribute(types.AttributeKeyDepositToken, transaction.DepositToken),
+		sdk.NewAttribute(types.AttributeKeyWithdrawalAmount, transaction.WithdrawalAmount),
+		sdk.NewAttribute(types.AttributeKeyDepositor, transaction.Depositor),
+		sdk.NewAttribute(types.AttributeKeyReceiver, transaction.Receiver),
+		sdk.NewAttribute(types.AttributeKeyWithdrawalChainID, transaction.WithdrawalChainId),
+		sdk.NewAttribute(types.AttributeKeyWithdrawalTxHash, transaction.WithdrawalTxHash),
+		sdk.NewAttribute(types.AttributeKeyWithdrawalToken, transaction.WithdrawalToken),
+		sdk.NewAttribute(types.AttributeKeySignature, transaction.Signature),
+		sdk.NewAttribute(types.AttributeKeyIsWrapped, strconv.FormatBool(transaction.IsWrapped))))
 }
--- x/bridge/keeper/transaction.go
+++ x/bridge/keeper/transaction.go
@@ -5,29 +5,11 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/hyle-team/bridgeless-core/v12/x/bridge/types"
-	"math/big"
-	"strconv"
 )
 
 func (k Keeper) SetTransaction(sdkCtx sdk.Context, transaction types.Transaction) {
 	tStore := prefix.NewStore(sdkCtx.KVStore(k.storeKey), types.Prefix(types.StoreTransactionPrefix))
 	tStore.Set(types.KeyTransaction(types.TransactionId(&transaction)), k.cdc.MustMarshal(&transaction))
-
-	sdkCtx.EventManager().EmitEvent(sdk.NewEvent(types.EventType_DEPOSIT_SUBMITTED.String(),
-		sdk.NewAttribute(types.AttributeKeyDepositTxHash, transaction.DepositTxHash),
-		sdk.NewAttribute(types.AttributeKeyDepositNonce, big.NewInt(int64(transaction.DepositTxIndex)).String()),
-		sdk.NewAttribute(types.AttributeKeyDepositChainId, transaction.DepositChainId),
-		sdk.NewAttribute(types.AttributeKeyDepositAmount, transaction.DepositAmount),
-		sdk.NewAttribute(types.AttributeKeyDepositBlock, big.NewInt(int64(transaction.DepositBlock)).String()),
-		sdk.NewAttribute(types.AttributeKeyDepositToken, transaction.DepositToken),
-		sdk.NewAttribute(types.AttributeKeyWithdrawalAmount, transaction.WithdrawalAmount),
-		sdk.NewAttribute(types.AttributeKeyDepositor, transaction.Depositor),
-		sdk.NewAttribute(types.AttributeKeyReceiver, transaction.Receiver),
-		sdk.NewAttribute(types.AttributeKeyWithdrawalChainID, transaction.WithdrawalChainId),
-		sdk.NewAttribute(types.AttributeKeyWithdrawalTxHash, transaction.WithdrawalTxHash),
-		sdk.NewAttribute(types.AttributeKeyWithdrawalToken, transaction.WithdrawalToken),
-		sdk.NewAttribute(types.AttributeKeySignature, transaction.Signature),
-		sdk.NewAttribute(types.AttributeKeyIsWrapped, strconv.FormatBool(transaction.IsWrapped))))
 
 }
 
